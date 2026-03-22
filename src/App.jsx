2775442bda3349780a8b8c14b9c2b238e9f895b7
Keng Puthearith <puthearithkeng7@gmail.com>
2026-03-23 02:39:41 +0700

    update pag

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './pages/Homepage.jsx';
-import Project from './pages/Projectpage.jsx';
-import About from './pages/Aboutpage.jsx';
-import Contact from './pages/Contactpage.jsx';
+import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
+import Home from './pages/homepage.jsx';
+import Project from './pages/projectpage.jsx';
+import About from './pages/aboutpage.jsx';
+import Contact from './pages/contactpage.jsx';
 import './App.css'
 
 function App() {
